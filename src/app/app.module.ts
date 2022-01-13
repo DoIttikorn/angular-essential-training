96d4bd0fba54093b57b9fa20cb8a3a583bd5b993
dodo <aseaways1234@gail.com>
2022-01-13 16:38:20 +0700

    learn NgModule and root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
-import { Ngmodule } from "@angular/core";
-
-@Ngmodule({
-  selector: [],
-
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+@NgModule({
+  imports: [BrowserModule], // is used to import other modules
+  declarations: [AppComponent], // is used to declare components, directives, pipes
+  bootstrap: [AppComponent], // is used to specify the root component, will be used to bootstrap the application
 })
 export class AppModule {}
